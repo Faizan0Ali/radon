fce96a7dbadcdcc320a55321521e8ec94ce5e194
Faizan0Ali <syedfaizanali898@gmail.com>
2022-06-01 19:38:30 +0530

    hello first changes

---
+++ src/routes/logger/logger.js
@@ -0,0 +1,14 @@
+// const endpoint = "https://www.functionup.org"
+// const batch = "radon"
+
+// const log = function() {
+//     console.log('I am inside the log function')
+// }
+const welcome = function(){
+    console.log('Welcome to my application. I am <name> and a part of FunctionUp Thorium cohort.')
+}
+
+// module.exports.endpoint = endpoint
+// module.exports.batch = batch
+module.exports.log = log
+module.exports.welcome=welcome
